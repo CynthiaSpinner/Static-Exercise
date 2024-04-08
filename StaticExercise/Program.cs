3a5f2a019409b7fa4e3e7c6bd88139f2727b9f93
Cynthia <CynthiaSpinner@gmail.com>
2024-04-08 10:34:25 -0500

    finished converter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(24);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(12);
+
+            Console.WriteLine($"{celsius}");
+            Console.WriteLine($"{fahrenheit}");
         }
     }
 }
